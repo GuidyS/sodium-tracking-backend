b5d996c3ab6f4954b41eb3155e8a84613a4ea57b
GuidyS <johnalex1137@gmail.com>
2026-03-14 04:34:25 +0700

    Update index.php

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,6 +1,6 @@
 <?php
 // ห้ามมีเว้นวรรคหรือบรรทัดว่างก่อน <?php เด็ดขาด
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://sodiumtracking.vercel.app");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, ngrok-skip-browser-warning");
