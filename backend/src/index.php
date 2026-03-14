f0d7421db3edd1b640f91baae0b075bcad837db7
GuidyS <johnalex1137@gmail.com>
2026-03-14 09:28:00 +0700

    Update index.php

--- backend/src/index.php
+++ backend/src/index.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: https://sodiumtracking.vercel.app");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, ngrok-skip-browser-warning");
+header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
