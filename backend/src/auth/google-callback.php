3ae95da8cac8258cad22ba24f8dff2b369c11375
GuidyS <johnalex1137@gmail.com>
2026-03-16 00:00:37 +0700

    9a74b96

--- backend/src/auth/google-callback.php
+++ backend/src/auth/google-callback.php
@@ -58,7 +58,7 @@ try {
             "user_id" => $userId,
             "full_name" => $userInfo->name,
             "email" => $userInfo->email,
-            "user_role" => $userRole,
+            "user_role" => $userRole
             "is_google" => true // 🌟 เพิ่มบรรทัดนี้เพื่อบอกหน้าบ้านว่าเป็น Google User
         ]));
 
@@ -71,4 +71,3 @@ try {
     header("Location: https://sodiumtracking.vercel.app/login?error=" . urlencode($e->getMessage()));
     exit;
 }
-?>
